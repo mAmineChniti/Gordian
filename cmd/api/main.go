3de9a858221e4f4cfefacc5965b3e10ee3905dbb
mAmineChniti <emin.chniti@esprit.tn>
2024-09-25 10:59:31 +0100

    added jwt auth

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/mAmineChniti/Gordian/internal/server"
 )
 
 func main() {
 
 	server := server.NewServer()
-
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
+	log.Print(fmt.Sprintf("Server is running on port %s", server.Addr))
 }
