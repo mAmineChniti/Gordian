df145274aab2998694207320f70957504f7b21fb
mAmineChniti <emin.chniti@esprit.tn>
2025-03-23 20:13:26 +0100

    added birthdate to user

--- internal/data/validator.go
+++ internal/data/validator.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -10,26 +11,52 @@ var validate *validator.Validate
 
 func init() {
 	validate = validator.New()
+	validate.RegisterValidation("birthdate", validateBirthdate)
 }
 
-func ValidateStruct(s any) (map[string]string, error) {
+func validateBirthdate(fl validator.FieldLevel) bool {
+	dateStr := fl.Field().String()
+	if dateStr == "" {
+		return false
+	}
+
+	birthdate, err := time.Parse("02/01/2006", dateStr)
+	if err != nil {
+		return false
+	}
+
+	now := time.Now()
+	age := now.Year() - birthdate.Year()
+
+	if now.Month() < birthdate.Month() || (now.Month() == birthdate.Month() && now.Day() < birthdate.Day()) {
+		age--
+	}
+
+	return age >= 18
+}
+
+func ValidateStruct(s any) (string, error) {
 	err := validate.Struct(s)
 	if req, ok := s.(UpdateRequest); ok {
-		if req.Username == "" && req.Email == "" && req.Password == "" && req.FirstName == "" && req.LastName == "" {
-			return nil, fmt.Errorf("empty update request")
+		if req.Username == "" && req.Email == "" && req.Password == "" && req.FirstName == "" && req.LastName == "" && req.Birthdate == "" {
+			return "", fmt.Errorf("empty update request")
 		}
 	}
 	if err != nil {
 		if _, ok := err.(*validator.InvalidValidationError); ok {
-			return nil, fmt.Errorf("invalid validation error: %w", err)
+			return "", fmt.Errorf("invalid validation error: %w", err)
 		}
 
 		validationErrors := err.(validator.ValidationErrors)
-		errorsMap := make(map[string]string)
-		for _, fieldErr := range validationErrors {
-			errorsMap[fieldErr.Field()] = fmt.Sprintf("failed on '%s' tag", fieldErr.Tag())
+		if len(validationErrors) > 0 {
+			fieldErr := validationErrors[0]
+			if fieldErr.Tag() == "birthdate" {
+				return fmt.Sprintf("%s must be in dd/mm/yyyy format and user must be 18+ years old", fieldErr.Field()), fmt.Errorf("validation errors")
+			} else {
+				return fmt.Sprintf("%s failed on '%s' tag", fieldErr.Field(), fieldErr.Tag()), fmt.Errorf("validation errors")
+			}
 		}
-		return errorsMap, fmt.Errorf("validation errors")
+		return "Unknown validation error", fmt.Errorf("validation errors")
 	}
-	return nil, nil
+	return "", nil
 }
